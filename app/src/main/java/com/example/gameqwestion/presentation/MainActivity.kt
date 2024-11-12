5d56350684ed2ccb7bee73efb117926cf1c4feff
OBER\Ober <frutria@yandex.ru>
2024-11-12 12:54:33 +0300

    Initial commit

---
+++ app/src/main/java/com/example/gameqwestion/presentation/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.gameqwestion.presentation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.gameqwestion.R
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+    }
+}
